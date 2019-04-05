e5d9269f109212b3e80dbd5e2c99a1659e9dade7
samjbasak <sam.basak@wearecodenation.com>
2019-04-05 16:24:51 +0100

    Initial commit

---
+++ practice.js
@@ -0,0 +1,184 @@
+/*console.log("   |   |   ")
+console.log("   |   |   ")
+console.log("   |   |   ")
+console.log("-----------")
+console.log("   |   |   ")
+console.log("   |   |   ")
+console.log("   |   |   ")
+console.log("-----------")
+console.log("   |   |   ")
+console.log("   |   |   ")
+console.log("   |   |   ")*/
+/*
+let space1 = 'x'
+let space2 = 'x'
+let space3 = ' '
+let space4 = ' '
+let space5 = 'x'
+let space6 = ' '
+let space7 = ' '
+let space8 = 'o'
+let space9 = 'x'
+
+console.log("   |   |   ")
+console.log(` ${space1} | ${space2} | ${space3} `)
+console.log("   |   |   ")
+console.log("-----------")
+console.log("   |   |   ")
+console.log(` ${space4} | ${space5} | ${space6} `)
+console.log("   |   |   ")
+console.log("-----------")
+console.log("   |   |   ")
+console.log(` ${space7} | ${space8} | ${space9} `)
+console.log("   |   |   ")
+
+
+if (space1 == 'x' && space2 == 'x' && space3 == 'x') {
+    console.log("x won")
+} else if (space1 == 'o' && space2 == 'o' && space3 == 'o') {
+    console.log("o won")
+} else {
+    console.log("No one has won yet")
+}*/
+
+
+
+
+/*
+let name = "Dan"
+
+if (name == "Sam") {
+    console.log("Your name is Sam")
+}
+else if (name == "Liam") {
+    console.log("Your name is Liam")
+}
+else {
+    console.log("What is your name?")
+}
+*/
+
+
+/*let myNumber = "5";
+
+if (myNumber === 5) {
+    console.log(myNumber)
+}
+else {
+    console.log("Your number sucks")
+}*/
+
+
+/*
+let age = 18;
+let country = "UK";
+
+if (age > 17 && country == "UK") {
+    console.log("You can have a drink")
+}
+else {
+    console.log("You can't drink yet")
+}
+*/
+
+
+/*
+const sayHi = (name) => {
+    console.log(`Hiya ${name}`);
+}
+
+sayHi("Sam");*/
+
+//sayHi();
+
+/*const addUp = (num1, num2) => {
+    return num1 + num2
+}
+
+let answer = addUp(4,5)
+
+console.log(addUp(answer, 5))
+*/
+/*
+const cashWithdrawal = (amount, accnum) => {
+    console.log(`Withdrawing ${amount} from account number ${accnum}`)
+    }
+cashWithdrawal(300, 50449921);
+cashWithdrawal(30, 50449921);
+cashWithdrawal(200, 50447921);
+*/
+/*
+const factorial = (n) => {
+    if ((n === 0) || (n === 1)){
+        return 1;
+    }
+    else {
+        return (n * factorial(n-1));
+    }
+}
+    console.log(factorial(33));
+    */
+
+    /*
+const multiplyNumbers = (num1, num2) => {
+    return num1 * num2
+}
+
+console.log(multiplyNumbers(6,7))
+*/
+
+//let myString = 'I said to Dan, "Dan you are great"'
+/*
+let coffeeOrder = ['Sam – Hot Chocolate', 'Stuart - Cortado', 'Dan - Latte'];
+
+console.log(coffeeOrder);
+*/
+
+/*let myFavouriteDrinks = ['Wine', 'Gin', 'Water']
+
+console.log(myFavouriteDrinks)
+
+myFavouriteDrinks.push("Squash")
+
+console.log(myFavouriteDrinks)
+
+myFavouriteDrinks.pop()
+
+console.log(myFavouriteDrinks)*/
+/*
+let person = {
+    name: 'Sam',
+    age: 29,
+    favouriteDrinks: ['Wine', 'Gin', 'Water'],
+    mood: "bad",
+    greeting () {
+        if (this.mood == "good") {
+            return "Hi, nice to see you!"}
+        else {
+            return "Hi, now go away!"}
+        },
+    };
+
+console.log(person.greeting())
+*/
+/*
+
+
+let person = {
+    name: 'Sam',
+    age: 29,
+    favouriteDrinks: ['Wine', 'Gin', 'Water'],
+    friendlyGreeting () {
+        return 'Hi, nice to see you!'
+    },
+    meanGreeting () {
+        return 'Hi, now go away!'
+    }
+};
+
+console.log(person.friendlyGreeting())*/
+
+
+console.log("9")
+console.log("Sam".toUpperCase())
+console.log(true)
